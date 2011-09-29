1cbf921a7d5fe06c5febd2b5f38673fda794c0a9
freire.tiago@gmail.com <freire.tiago@gmail.com@d4fb2844-59ee-5d09-9d59-799a29e9ae77>
2011-09-29 18:52:11 +0000

    exemplo de crud e validação

--- menuduchef/admin/gerenciar_cidades.php
+++ menuduchef/admin/gerenciar_cidades.php
@@ -1,8 +1,49 @@
 <?
 require("../php/lib/config.php");
 
-$cidades = Cidade::all();
+$obj = new Cidade();
 
+$data = $_POST ? $_POST : ($_GET ? $_GET : 0);
+$msgError = "";
+
+if($data) {
+	$id = array_key_exists("id", $data) ? $data["id"] : 0;
+	$action = array_key_exists("action", $data) ? $data["action"] : 0;
+	unset($data["action"]);
+
+	if($id) {
+		$obj = Cidade::find($id);
+	}
+
+	if($action) {
+		if($action == "create" || $action == "update") {
+			if($obj->is_invalid()) {
+				$msgError = $obj->errors->full_messages();
+			}
+		}
+		switch($action) {
+			case "create":
+				$obj = new Cidade($data);
+				$obj->save();
+				break;
+
+			case "update":
+				$obj->update_attributes($data);
+				break;
+
+			case "delete":
+				$obj->delete();
+				break;
+
+			default:
+				break;
+		}
+
+		$obj = new Cidade();
+	}
+}
+
+$itens = Cidade::all(array("order" => "nome asc"));
 ?>
 
 <html>
@@ -12,12 +53,26 @@ $cidades = Cidade::all();
 		<style type="text/css">
 			table { border-collapse: collapse; }
 			table tr td, table tr th { border: #000 solid 1px; }
+			.error { float: left; width: 50%; background-color: red; color: white; font-weight: bold; padding: 2px; }
 		</style>
 	</head>
 	<body>
-		<h2>Cadastrar</h2>
-		<form action="?action=cadastrar" method="post">
 
+		<? if($msgError) {
+			foreach($msgError as $msg) {
+		?>
+		<div class="error"><?= $msg ?></div>
+		<? } ?>
+		<br clear="all" />
+		<? } ?>
+
+		<h2>Gerenciar Cidades</h2>
+		<form action="" method="post">
+			Nome<br />
+			<input type="hidden" name="action" value="<?= $obj->id ? "update" : "create" ?>" />
+			<input type="hidden" name="id" value="<?= $obj->id ?>" />
+			<input type="text" name="nome" value="<?= $obj->nome ?>" maxlength="100" /><br /><br />
+			<input type="submit" value="<?= $obj->id ? "Modificar" : "Criar" ?>" />
 		</form>
 		<hr />
 		<h2>Cidades Cadastradas</h2>
@@ -28,21 +83,21 @@ $cidades = Cidade::all();
 				<th>Excluir</th>
 			</tr>
 			<?
-			if($cidades) {
-				foreach($cidades as $cidade) {
-			?>
-			<tr>
-				<td><?= $cidade->nome ?></td>
-				<td><a href="?id=<?= $cidade->id ?>">Modificar</a></td>
-				<td><a href="?id=<?= $cidade->id ?>&action=excluir">Excluir</a></td>
-			</tr>
-			<?
+			if($itens) {
+				foreach($itens as $item) {
+					?>
+					<tr>
+						<td><?= $item->nome ?></td>
+						<td><a href="?id=<?= $item->id ?>">Modificar</a></td>
+						<td><a href="?id=<?= $item->id ?>&action=delete" onclick="return window.confirm('Confirmar exclusão?')">Excluir</a></td>
+					</tr>
+					<?
 				}
 			} else {
-			?>
-			<tr>
-				<td colspan="3">Nenhuma cidade cadastrada</td>
-			</tr>
+				?>
+				<tr>
+					<td colspan="3">Nenhuma cidade cadastrada</td>
+				</tr>
 			<? } ?>
 		</table>
 	</body>
--- menuduchef/php/model/Cidade.php
+++ menuduchef/php/model/Cidade.php
@@ -1,11 +1,18 @@
 <?
 
 class Cidade extends ActiveRecord\Model {
+
 	static $table_name = "cidade";
+
 	static $has_many = array(
-	    array("bairros", "foreign_key" => "id_cidade", "class_name" => "Bairro"),
-            array("restaurantes", "foreign_key" => "id_cidade", "class_name" => "Restaurante")
+		array("bairros", "foreign_key" => "id_cidade", "class_name" => "Bairro"),
+		array("restaurantes", "foreign_key" => "id_cidade", "class_name" => "Restaurante")
 	);
+
+	static $validates_presence_of = array(
+		array('nome', 'message' => 'obrigatório')
+	);
+
 }
 
 ?>
