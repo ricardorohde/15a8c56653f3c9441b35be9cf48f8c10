2767eccdcfcfbf1bed470cb786374eee60e3ac7e
paulo@agenciabiro.com.br <paulo@agenciabiro.com.br@d4fb2844-59ee-5d09-9d59-799a29e9ae77>
2011-09-30 14:14:43 +0000

    gerenciar_administrador criado

--- menuduchef/php/model/RestauranteTemTipo.php
+++ menuduchef/php/model/RestauranteTemTipo.php
@@ -1,6 +1,6 @@
 <?
 
-class Restaurante_Tem_Tipo extends ActiveRecord\Model {
+class RestauranteTemTipo extends ActiveRecord\Model {
 	static $table_name = "restaurante_tem_tipo";
 	static $belongs_to = array(
 		array("restaurante", "foreign_key" => "id_restaurante"),
--- menuduchef/php/model/TipoRestaurante.php
+++ menuduchef/php/model/TipoRestaurante.php
@@ -1,9 +1,9 @@
 <?
 
-class Tipo_Restaurante extends ActiveRecord\Model {
+class TipoRestaurante extends ActiveRecord\Model {
 	static $table_name = "tipo_restaurante";
 	static $has_many = array(
-	    array("restaurante_tem_tipos", "foreign_key" => "id_tipo", "class_name" => "Restaurante_Tem_Tipo"),
+	    array("restaurante_tem_tipos", "foreign_key" => "id_tipo", "class_name" => "RestauranteTemTipo"),
             array("restaurante_tem_tipos"),
             array("restaurantes", 'through' => 'restaurante_tem_tipos', "foreign_key" => "id_tipo", "class_name" => "Restaurante")
 	);
