c2420a8f81527b6c6810797a1722973f331759b5
paulo@agenciabiro.com.br <paulo@agenciabiro.com.br@d4fb2844-59ee-5d09-9d59-799a29e9ae77>
2011-09-30 14:57:53 +0000

    saih tirando os underlines das classes e dos nomes dos arquivos

--- menuduchef/php/model/TipoProduto.php
+++ menuduchef/php/model/TipoProduto.php
@@ -2,6 +2,10 @@
 
 class TipoProduto extends ActiveRecord\Model {
 	static $table_name = "tipo_produto";
+        static $belongs_to = array(
+		array("restaurante", "foreign_key" => "id_restaurante")
+	);
+        
 	static $has_many = array(
 	    array("produto_tem_tipos", "foreign_key" => "id_tipo", "class_name" => "ProdutoTemTipo"),
             array("produto_tem_tipos"),
