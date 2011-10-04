668ed29f26f2f2eed429c13ff727be39bef56deb
paulo@agenciabiro.com.br <paulo@agenciabiro.com.br@d4fb2844-59ee-5d09-9d59-799a29e9ae77>
2011-10-04 14:42:01 +0000

    pequenas alteracoes

--- menuduchef/php/model/Consumidor.php
+++ menuduchef/php/model/Consumidor.php
@@ -3,7 +3,6 @@
 class Consumidor extends ActiveRecord\Model {
 	static $table_name = "consumidor";
 	static $belongs_to = array(
-		array("cidade", "foreign_key" => "id_cidade"),
                 array("bairro", "foreign_key" => "id_bairro")
 	);
         static $has_many = array(
--- menuduchef/php/model/TipoProduto.php
+++ menuduchef/php/model/TipoProduto.php
@@ -2,9 +2,6 @@
 
 class TipoProduto extends ActiveRecord\Model {
 	static $table_name = "tipo_produto";
-        static $belongs_to = array(
-		array("restaurante", "foreign_key" => "id_restaurante")
-	);
         
 	static $has_many = array(
 	    array("produto_tem_tipos", "foreign_key" => "id_tipo", "class_name" => "ProdutoTemTipo"),
